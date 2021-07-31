d1f10eb8107525e004edf6573c98f6e61e974b96
najraham <anishamaharjan4@gmail.com>
2021-08-01 00:05:09 +0545

    email validation in create user

--- imports/api/methods/userMethods.js
+++ imports/api/methods/userMethods.js
@@ -2,10 +2,16 @@ import { Accounts } from 'meteor/accounts-base'
 
 Meteor.methods({
     registerUser(user) {
-        Accounts.createUser({
-            username: user.username,
-            email: user.email,
-            password: user.password
-        })
+        const userWithEmail = Accounts.findUserByEmail({email: user.email})
+        if(userWithEmail) {
+            throw new Meteor.Error('bad', 'Email is already taken')
+        }
+        else {
+            Accounts.createUser({
+                username: user.username,
+                email: user.email,
+                password: user.password
+            })
+        }
     }
 });
